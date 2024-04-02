0184108bc6de622fe6dc605ea96c95cb88b79ab3
Jenna Jones <jljones121@gmail.com>
2024-04-02 19:57:56 +0000

    ..

--- pathfinder.c
+++ pathfinder.c
@@ -1,53 +1,46 @@
 #include "main.h"
 
-void free_array(char **array);
+/**
+*env_pathfinder - returns path to native commands (fun_fact: pathfinder is one of the toughest Army schools)
+*
+*
+*Return: path for command inputted
+*/
 
-char *env_pathfinder(char **cmd) {
-    int pos = 0;
-    char *path = getenv("PATH");
-    char **path_az, *tgt;
+char env_pathfinder(void)
+{
+	int pos = 0;
+	char *path = get_env("PATH");
+	char **path_az, *tgt;
 
-    if (path == NULL) {
-        return NULL;
-    }
+	if (strncmp(cmd[0], "./", 2) == 0 || cmd[0][0] == "/" || strncmp(cmd[0], "../". 3))
+	{
+		if (access(cmd[0], F_OK) == 0)
+		{
+			return (strdup(cmd[0]);
+		}
+	}
 
-    if (strncmp(cmd[0], "./", 2) == 0 || cmd[0][0] == '/' || strncmp(cmd[0], "../", 3) == 0) {
-        if (access(cmd[0], F_OK) == 0) {
-            return strdup(cmd[0]);
-        }
-    }
+	if (!path)
+	{
+		return (NULL);
+	}
+	path_az = container(path, ':');
 
-    path_az = container(path, ':');
+	for (;path_dir[pos]; pos++)
+	{
+		tgt = malloc(MAX_INPUT);
+		strcpy(tgt, path_dir[i]);
+		strcat(tgt, "/");
+		strcat(tgt, cmd[0]);
 
-    for (; path_az[pos]; pos++) {
-        tgt = malloc(MAX_INPUT);
-        if (tgt == NULL) {
-            perror("malloc failed");
-            exit(EXIT_FAILURE);
-        }
-        strcpy(tgt, path_az[pos]);
-        strcat(tgt, "/");
-        strcat(tgt, cmd[0]);
-
-        if (access(tgt, F_OK) == 0) {
-            free_array(path_az);
-            return tgt;
-        }
-        free(tgt);
-    }
-
-    free_array(path_az);
-    return NULL;
+		if (access(tgt, F_OK) == 0)
+		{
+			free_array(path_az);
+			return (tgt);
+		}
+		free(tgt);
+	}
+	free_array(path_az);
+	return (NULL);
 }
-
-void free_array(char **array) {
-    int i;
-    if (array == NULL)
-        return;
-    
-    for (i = 0; array[i] != NULL; ++i) {
-        free(array[i]);
-    }
-    free(array);
-}
-
