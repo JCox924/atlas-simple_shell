3bde08427007e5688b711b3c0b905bf4e35cb26e
Jenna Jones <jljones121@gmail.com>
2024-04-02 17:08:21 +0000

    pathfinder

--- pathfinder.c
+++ pathfinder.c
@@ -5,9 +5,23 @@
 
 #define MAX_INPUT 256
 
+void free_array(char **array) {
+    int i;
+    if (array == NULL)
+        return;
+    
+    for (i = 0; array[i] != NULL; ++i) {
+        free(array[i]);
+    }
+    free(array);
+}
 
 char *env_pathfinder(char **cmd) {
     char *path = getenv("PATH");
+
+    if (path == NULL) {
+        return NULL;
+    }
 
     if (strncmp(cmd[0], "./", 2) == 0 || cmd[0][0] == '/' || strncmp(cmd[0], "../", 3) == 0) {
         if (access(cmd[0], F_OK) == 0) {
@@ -15,10 +29,5 @@ char *env_pathfinder(char **cmd) {
         }
     }
 
-    if (!path) {
-        return NULL;
-    }
-
-    return NULL;
+    return (NULL);
 }
-
